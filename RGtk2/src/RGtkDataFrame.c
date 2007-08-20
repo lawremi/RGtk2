302f92e6610ad5577d2eb3b005bec45c94905d0a
Michael Lawrence <michafla@gene.com>
2007-08-20 20:18:47 +0000

    Fixed typo.

--- RGtk2/src/RGtkDataFrame.c
+++ RGtk2/src/RGtkDataFrame.c
@@ -601,7 +601,7 @@ r_gtk_data_frame_set_default_sort_func (GtkTreeSortable        *sortable,
                                               gpointer                user_data,
                                               GtkDestroyNotify        destroy_func)
 {
-    warning ("gtk_tree_sortable_set_default_sort_func is not supported by the RGtkDataModel model.",);
+    warning("gtk_tree_sortable_set_default_sort_func is not supported by the RGtkDataModel model.");
 }
 
 
