47d524be74f6ad8203176fa9ac315a166025a80d
Michael Lawrence <michafla@gene.com>
2008-04-18 23:13:30 +0000

    Further tweaks to the win32 event loop.

--- RGtk2/src/Rgtk.c
+++ RGtk2/src/Rgtk.c
@@ -20,7 +20,7 @@ R_gtk_eventHandler(void *userData)
 #ifdef G_OS_WIN32
 
 /* On Windows, run the GTK+ event loop in a separate thread, synchronizing
-   with the Windows event loop.
+   through the Windows event loop on the main thread.
    This currently doesn't handle timed tasks.
    More to come later on an overhaul of the R event loop.
 */
@@ -30,14 +30,18 @@ R_gtk_eventHandler(void *userData)
 DWORD WINAPI R_gtk_thread_proc(LPVOID lpParam) {
   while(1) {
     if (gtk_events_pending())
-      PostMessage((HWND)lpParam, RGTK2_ITERATE, NULL, NULL);
+      PostMessage((HWND)lpParam, RGTK2_ITERATE, 0, 0);
+    Rprintf("Thread\n");
   }
+  return 0;
 }
 
-void
-R_gtk_win_proc()
+LRESULT CALLBACK
+R_gtk_win_proc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
+  Rprintf("Event loop\n");
   R_gtk_eventHandler(NULL);
+  return 0;
 }
 #endif
 
@@ -68,16 +72,16 @@ R_gtkInit(long *rargc, char **rargv, Rboolean *success)
   }
 #else
   /* Create a dummy window for receiving messages */
+  LPCTSTR class = "RGtk2";
   HINSTANCE instance = GetModuleHandle(NULL);
-  WNDCLASS wndclass = { 0, R_gtk_win_proc, 0, 0, instance, NULL, NULL, NULL, NULL,
-                        "RGtk2" };
-  ATOM atom = RegisterClass(&wndclass);
-  HWND win = CreateWindow(atom, NULL, NULL, 1, 1, 1, 1, HWND_MESSAGE, NULL,
-                          instance, NULL);
+  WNDCLASS wndclass = { 0, R_gtk_win_proc, 0, 0, instance, NULL, 0, 0, NULL,
+                        class };
+  RegisterClass(&wndclass);
+  HWND win = CreateWindow(class, NULL, 0, 1, 1, 1, 1, NULL, NULL, instance, NULL);
 
   /* Create a thread that will post messages to our window on this thread */
   HANDLE thread = CreateThread(NULL, 0, R_gtk_thread_proc, win, 0, NULL);
-  SetThreadPriority(thread, THREAD_PRIORITY_IDLE);
+  /*SetThreadPriority(thread, THREAD_PRIORITY_IDLE);*/
 #endif
 
   R_GTK_TYPE_PARAM_SEXP;
