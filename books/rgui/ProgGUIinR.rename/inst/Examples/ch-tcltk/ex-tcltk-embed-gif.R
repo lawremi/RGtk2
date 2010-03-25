c2c9a8c236b7e64fc47e873e38d75174c402a9a7
John Verzani <gwidgetsrgtk@gmail.com>
2010-03-25 02:15:20 +0000

    fix ProgGUIinR mess

--- books/rgui/ProgGUIinR.rename/inst/Examples/ch-tcltk/ex-tcltk-embed-gif.R
+++
@@ -1,6 +0,0 @@
-##http://code.activestate.com/recipes/117247/
-  
-f <- convertGif <- function(img) {
-  tcl("package", "require","base64")
-  as.character(.Tcl(paste("base64::encode [read [open ", img, "]]", sep="")))
-}
