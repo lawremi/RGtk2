94161a37c70a4c3a72ecf0d61e8ec527e9b921b9
Michael Lawrence <michafla@gene.com>
2010-08-22 22:01:32 +0000

    Demos of new functionality in 2.20 + GIO

--- RGtk2/demo/builder.R
+++ RGtk2/demo/builder.R
@@ -10,25 +10,11 @@ about_activate <- function(action)
   about_dlg$hide()
 }
 
-## GtkBuilder can't find any types, so we init them here
-## Something to do with the dynamic linking
-gtkListStoreGetType()
-gtkUIManagerGetType()
-gtkActionGroupGetType()
-gtkWindowGetType()
-gtkVBoxGetType()
-gtkMenuBarGetType()
-gtkScrolledWindowGetType()
-gtkTreeViewGetType()
-gtkCellRendererTextGetType()
-gtkStatusbarGetType()
-##
-
 builder <- gtkBuilder()
 filename <- system.file("ui", "demo.ui", package = "RGtk2")
 res <- builder$addFromFile(filename)
 if (!is.null(res$error))
-  stop("ERROR: ", error$message)
+  stop("ERROR: ", res$error$message)
 builder$connectSignals(NULL)
 window <- builder$getObject("window1")
 
