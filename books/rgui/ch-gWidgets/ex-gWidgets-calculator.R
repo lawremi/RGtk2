d2306f3140bc96ea215f576cbd78ba17632a7120
John Verzani <gwidgetsrgtk@gmail.com>
2011-01-27 19:03:41 +0000

    edits to gWidgets

--- books/rgui/ch-gWidgets/ex-gWidgets-calculator.R
+++ books/rgui/ch-gWidgets/ex-gWidgets-calculator.R
@@ -13,7 +13,7 @@ library(gWidgets)
 buttons <- rbind(c(7:9, "(", ")"),
                  c(4:6, "*", "/"),
                  c(1:3, "+", "-"))
-bList <- list()
+#
 w <- gwindow("glayout for a calculator", visible=FALSE)
 g <- ggroup(cont=w, expand=TRUE, horizontal=FALSE)
 tbl <- glayout(cont=g, spacing=2)
@@ -22,6 +22,8 @@ tbl[1, 1:5, anchor=c(-1,0)] <-          # span 5 columns
   (eqnArea <- gedit("", cont=tbl))
 tbl[2, 1:5, anchor=c(1,0)] <- 
   (outputArea <- glabel("", cont=tbl))
+#
+bList <- list()
 for(i in 3:5) {
   for(j in 1:5) {
     val <- buttons[i-2, j]
@@ -31,29 +33,29 @@ for(i in 3:5) {
 tbl[6,2] <- (bList[["0"]] <- gbutton("0", cont=tbl))
 tbl[6,3] <- (bList[["."]] <- gbutton(".", cont=tbl))
 tbl[6,4:5] <- (eqButton <- gbutton("=", cont=tbl))
-
+#
 visible(w) <- TRUE
 
 
 ###################################################
 ### chunk number 3: 
 ###################################################
-#line 51 "ex-gWidgets-calculator.Rnw"
+#line 53 "ex-gWidgets-calculator.Rnw"
 addButton <- function(h, ...) {
   curExpr <- svalue(eqnArea)
   newChar <- svalue(h$obj)              # the button's value
   svalue(eqnArea) <- paste(curExpr, newChar, sep="")
-  svalue(outputArea) <- ""             # clear label 
+  svalue(outputArea) <- ""              # clear label 
 }
-out <- sapply(bList, function(i) 
-              addHandlerChanged(i, handler=addButton))
-
+#out <- sapply(bList, function(i) 
+#              addHandlerChanged(i, handler=addButton))
+sapply(bList, addHandlerChanged, handler=addButton)
 
 
 ###################################################
 ### chunk number 4: 
 ###################################################
-#line 65 "ex-gWidgets-calculator.Rnw"
+#line 67 "ex-gWidgets-calculator.Rnw"
 addHandlerClicked(eqButton, handler = function(h,...) {
   curExpr <- svalue(eqnArea)
   out <- try(capture.output(eval(parse(text=curExpr))), 
