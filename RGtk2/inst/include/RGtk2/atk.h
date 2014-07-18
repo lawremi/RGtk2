fe7b45cee0227e802fc15d762651f3b0bdf7d778
Michael Lawrence <michafla@gene.com>
2014-07-18 06:40:33 -0700

    apparently ATK now has a version macro

--- RGtk2/inst/include/RGtk2/atk.h
+++ RGtk2/inst/include/RGtk2/atk.h
@@ -6,14 +6,16 @@
 #include <atk/atk.h>
 #include <atk/atk-enum-types.h>
 
-/* Unlike the other libraries, ATK does not provide version information,
-   so we are just fixing ATK at 1.10.0 for now (all later stuff disabled).
-   This does not bother me too much, since I have yet to find a use for ATK */
+/* Unlike the other libraries, ATK did not always provide version information,
+   so we assume 1.10.0 when the macro is not present.
+*/
+#ifndef ATK_CHECK_VERSION
 #define ATK_CHECK_VERSION(major,minor,micro)    \
     (1 > (major) || \
      (1 == (major) && 10 > (minor)) || \
      (1 == (major) && 10 == (minor) && \
       0 >= (micro)))
+#endif
 
 #include <RGtk2/atkUserFuncs.h>
 #include <RGtk2/atkClasses.h>
