456e5e971a7aaddddc6f2c50a83d238dd9f7cac0
larman <michafla@gene.com>
2023-02-02 04:33:36 -0800

    drop atk_text_free_ranges() which is now present in the win32 binary
    related to #9

--- RGtk2/src/atkManuals.c
+++ RGtk2/src/atkManuals.c
@@ -20,26 +20,3 @@ S_atk_editable_text_insert_text(USER_OBJECT_ s_object, USER_OBJECT_ s_string, US
 	UNPROTECT(1);
         return(_result);
 }
-
-/* This function is not provided in the win32 build of the library */
-#ifdef WIN32
-void
-atk_text_free_ranges (AtkTextRange **ranges)
-{
-  AtkTextRange **first = ranges;
-
-  if (ranges)
-    {
-      while (*ranges)
-        {
-          AtkTextRange *range;
-
-          range = *ranges;
-          (*ranges)++;
-          g_free (range->content);
-          g_free (range);
-        }
-      g_free (first);
-    }
-}
-#endif
